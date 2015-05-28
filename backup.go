8a81b12d703e3efc86dca294520b6c6f8712ea9c
Venu <venugopalkandru@users.noreply.github.com>
2015-05-28 11:48:53 +0530

    Update backup.go

--- backup.go
+++ backup.go
@@ -11,55 +11,55 @@ import (
 )
 
 type BackupEngineInfo struct {
-    c *C.rocksdb_backup_engine_info_t
+	c *C.rocksdb_backup_engine_info_t
 }
 
 func (self *BackupEngineInfo) GetCount() int {
-    return int(C.rocksdb_backup_engine_info_count(self.c))
+	return int(C.rocksdb_backup_engine_info_count(self.c))
 }
 
 // BackupEngine is a reusable handle to a RocksDB Backup, created by
 // OpenBackupEngine
 type BackupEngine struct {
-    c *C.rocksdb_backup_engine_t
-    path string
-    opts *Options
+	c *C.rocksdb_backup_engine_t
+	path string
+	opts *Options
 }
 
 
 // OpenBackupEngine opens a backup engine with specified options
 func OpenBackupEngine(opts *Options, path string) (*BackupEngine, error) {
 	var cErr *C.char
-    cpath := C.CString(path)
+	cpath := C.CString(path)
 	defer C.free(unsafe.Pointer(cpath))
 
-    be := C.rocksdb_backup_engine_open(opts.c, cpath, &cErr)
-    if cErr != nil {
-        defer C.free(unsafe.Pointer(cErr))
-        return nil, errors.New(C.GoString(cErr))
-    }
+	be := C.rocksdb_backup_engine_open(opts.c, cpath, &cErr)
+	if cErr != nil {
+		defer C.free(unsafe.Pointer(cErr))
+		return nil, errors.New(C.GoString(cErr))
+	}
 
-    return &BackupEngine{
-        c: be,
-        path: path,
-        opts: opts,
-    }, nil
+	return &BackupEngine{
+		c: be,
+		path: path,
+		opts: opts,
+	}, nil
 }
 
 func (self *BackupEngine) CreateNewBackup(db *DB) error {
 	var cErr *C.char
 
-    C.rocksdb_backup_engine_create_new_backup(self.c, db.c, &cErr)
-    if cErr != nil {
-        defer C.free(unsafe.Pointer(cErr))
-        return errors.New(C.GoString(cErr))
-    }
+	C.rocksdb_backup_engine_create_new_backup(self.c, db.c, &cErr)
+	if cErr != nil {
+		defer C.free(unsafe.Pointer(cErr))
+		return errors.New(C.GoString(cErr))
+	}
 
-    return nil
+	return nil
 }
 
 func (self *BackupEngine) GetInfo() *BackupEngineInfo {
-    return &BackupEngineInfo{
-        c: C.rocksdb_backup_engine_get_backup_info(self.c),
-    }
+	return &BackupEngineInfo{
+		c: C.rocksdb_backup_engine_get_backup_info(self.c),
+	}
 }
